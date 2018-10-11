c0ac28232cc6da4903877627f7284833434ee07c
CB_ATL_Cohort_2_Brittany_Lewis <cb_atl_cohort_2_brittany_lewis@CBs-MacBook-Air-7.local>
2018-10-11 17:59:57 -0400

    Commit 5 - Committing because I'm suppose to nothing has changed but the time

--- app.js
+++ app.js
@@ -3,7 +3,19 @@
 // alert("Hey!")
 // Testing Javascript functionality in connection with HTML and CSS
 
-$('.beyonceQs').click(function(){
-    $(this).toggleClass('flipped');
-  });
+// $('#bey1').click(function() {
+//     $(this).toggleClass('flipped');
+//   });
 
+//   $('.twohundreds').click(function() {
+//     $(this).toggleClass('flipped');
+//   });
+//   $('.threehundreds').click(function() {
+//     $(this).toggleClass('flipped');
+//   });
+//   $('.fourhundreds').click(function() {
+//     $(this).toggleClass('flipped');
+//   });
+//   $('.fivehundreds').click(function() {
+//     $(this).toggleClass('flipped');
+//   });
