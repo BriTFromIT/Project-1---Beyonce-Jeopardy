a80410f230323fcb4ac19a4712a0fbe91c7b760c
CB_ATL_Cohort_2_Brittany_Lewis <cb_atl_cohort_2_brittany_lewis@CBs-MacBook-Air-7.local>
2018-10-12 14:27:13 -0400

    Commit 8 - working on javascript

--- app.js
+++ app.js
@@ -6,3 +6,25 @@
 // $('.modalfade').on('shown.bs.modal', function () {
 //     $('#modalfad').trigger('focus')
 //   })\
+
+let playerOne = document.querySeletor(".player1");
+let playerTwo = document.querySelector(".player2");
+let bey1 = document.querySelector("#bey1");
+let bey2 = document.querySelector("#bey2");
+let bey3 = document.querySelector("#bey3");
+let sfOne = document.querySelector("#sF1");
+let sfTwo = document.querySelector("#sF2");
+let sfThree = document.querySelector("#sF3");
+let yonceOne = document.querySelector("#yonce1");
+let yonceTwo = document.querySelector("#yonce2");
+let yonceThree = document.querySelector("#yonce3");
+let score = document.querySelector(".score");
+let sonicCoins = 0;
+
+playerOne.addEventListener("click", function() {
+    .sonicCoins + 100
+    score.innerHTML = sonicCoins
+ });
+
+ 
+
