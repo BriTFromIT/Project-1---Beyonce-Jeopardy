fd6f2e355a6aae5354ac75829dbd367b6d40eb43
CB_ATL_Cohort_2_Brittany_Lewis <cb_atl_cohort_2_brittany_lewis@CBs-MacBook-Air-7.local>
2018-10-12 15:06:01 -0400

    Commit 9 - still fiddling with javascript... commiting for safety

--- app.js
+++ app.js
@@ -7,8 +7,8 @@
 //     $('#modalfad').trigger('focus')
 //   })\
 
-let playerOne = document.querySeletor(".player1");
-let playerTwo = document.querySelector(".player2");
+let playerOne = document.querySeletor("#player1");
+let playerTwo = document.querySelector("#player2");
 let bey1 = document.querySelector("#bey1");
 let bey2 = document.querySelector("#bey2");
 let bey3 = document.querySelector("#bey3");
@@ -20,11 +20,23 @@ let yonceTwo = document.querySelector("#yonce2");
 let yonceThree = document.querySelector("#yonce3");
 let score = document.querySelector(".score");
 let sonicCoins = 0;
+let h7p1 = document.querySelector("h7p1")
+let h7p2 = document.querySelector("h7p1")
 
 playerOne.addEventListener("click", function() {
-    .sonicCoins + 100
-    score.innerHTML = sonicCoins
+    sonicCoins += 100
+    h7p1.innerHTML = sonicCoins
  });
 
+//  player1.addEventListener('click', (event) => {
+//     console.log(sonicCoins.value)
  
+ 
+//     if (player1.value == ".player1") {
+//         console.log('sonicCoins')
+//     } else {
+//         console.log("no")
+//     }
+ 
+//  })
 
