9e4aa4dbb01af9c51a8313410fd97e6804c6d0b1
CB_ATL_Cohort_2_Brittany_Lewis <cb_atl_cohort_2_brittany_lewis@CBs-MacBook-Air-7.local>
2018-10-15 11:18:45 -0400

    Commit 13 - Worked on more javascript

--- app.js
+++ app.js
@@ -9,6 +9,13 @@
 
 // let playerOne = document.querySeletor("#player1");
 // let playerTwo = document.querySelector("#player2");
+
+let score1 = document.querySelector("#score1");
+let score2 = document.querySelector("#score2");
+let sonicCoins = 0;
+
+
+
 let bey1p1 = document.querySelector("#bey1p1");
 let bey1p2 = document.querySelector("#bey1p2");
 
@@ -122,29 +129,37 @@ let yonce2p2 = document.querySelector("#yonce2p2");
 
 
 yonce2p1.addEventListener('click', function () {
-    sonicCoins += 100
+    sonicCoins += 200
     console.log(sonicCoins)
     score1.innerHTML = sonicCoins
 })
 yonce2p2.addEventListener('click', function () {
-    sonicCoins += 100
+    sonicCoins += 200
+    console.log(sonicCoins)
+    score2.innerHTML = sonicCoins 
+});
+
+let yonce3p1 = document.querySelector("#yonce3p1");
+let yonce3p2 = document.querySelector("#yonce3p2");
+
+
+yonce3p1.addEventListener('click', function () {
+    sonicCoins += 300
+    console.log(sonicCoins)
+    score1.innerHTML = sonicCoins
+})
+yonce3p2.addEventListener('click', function () {
+    sonicCoins += 300
     console.log(sonicCoins)
     score2.innerHTML = sonicCoins 
 });
 
 
-
-let sfOne = document.querySelector("#sF1");
-let sfTwo = document.querySelector("#sF2");
-let sfThree = document.querySelector("#sF3");
-let yonceOne = document.querySelector("#yonce1");
-let yonceTwo = document.querySelector("#yonce2");
-let yonceThree = document.querySelector("#yonce3");
-let score1 = document.querySelector("#score1");
-let score2 = document.querySelector("#score2");
-let sonicCoins = 0;
-let h7p1 = document.querySelector("#h7p1");
-let h7p2 = document.querySelector("#h7p2");
+// let score1 = document.querySelector("#score1");
+// let score2 = document.querySelector("#score2");
+// let sonicCoins = 0;
+// let h7p1 = document.querySelector("#h7p1");
+// let h7p2 = document.querySelector("#h7p2");
 
 
 
